dc2d9b6d8ac784f927b1e1fc0b8ab17802302800
Jelo777 <junheef1@gmail.com>
2023-09-19 09:47:58 +0900

    프로젝트 생성

---
+++ cuddly/src/test/java/com/kh/cuddly/CuddlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.cuddly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuddlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
