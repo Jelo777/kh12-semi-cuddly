c86e1900a13501ef0c27ba7b09ef964e974b4949
wijaemin <minialp2002@naver.com>
2023-09-25 21:51:22 +0900

    아이디 찾기 일단 실패

--- cuddly/src/main/java/com/kh/cuddly/dao/CertDao.java
+++ cuddly/src/main/java/com/kh/cuddly/dao/CertDao.java
@@ -5,7 +5,7 @@ import com.kh.cuddly.dto.CertDto;
 public interface CertDao {
 	void insert(CertDto certDto);
 	boolean delete(String certEmail);
-	boolean deleteOver5min();
+	boolean deleteOver10min();
 	CertDto selectOne(String certEmail);
-	CertDto selectOneIn5min(String certEmail);
+	CertDto selectOneIn10min(String certEmail);
 }
